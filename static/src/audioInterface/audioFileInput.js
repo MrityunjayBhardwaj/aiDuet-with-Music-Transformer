98835b722336dd6cd6371329c0963cffd1831595
Mrityunjay Bhardwaj <mrityunjaybhardwaj99@gmail.com>
2020-08-31 07:00:44 +0530

    added input file tage to our upload btn

--- static/src/audioInterface/audioFileInput.js
+++ static/src/audioInterface/audioFileInput.js
@@ -11,10 +11,30 @@ export default class audioLoader extends EventEmitter{
 
 		const fillText = document.createElement('div')
 		fillText.id = 'fillText'
-        fillText.innerHTML = '<div id="voice"></div> <div id="play">audio file</div>'
+
+        // fillText.innerHTML = '<div id="voice"></div> <div id="play">audio file<input type="file" id="fileInput"></span></div>'
+		fillText.innerHTML = 
+		`
+		<label class="button" id="btnUpload" disabled>
+			<span id="play" >Upload file <input type="file" id="fileInput"></span></span>
+			<span class="loading">Transcribing...</span>
+		</label>
+		
+		`
 		audioInput.appendChild(fillText)
 
         audioInput.classList.add('clickable')
+
+
+		const fileInput = document.querySelector('#fileInput');
+
+		fileInput.addEventListener('change', (w)=>{
+
+			console.log('file is uploaded', w)
+			this.emit('uploaded', w)
+
+		})
+
 
         audioInput.addEventListener('click', () => {
 
